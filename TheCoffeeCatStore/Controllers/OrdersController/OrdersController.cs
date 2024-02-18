a0a26d43ecfc0ec134a43d199c50ed4a38293c3f
Duy1408 <duynguyen08142001@gmail.com>
2024-02-18 14:17:39 +0700

    CRUD-Order

--- TheCoffeeCatStore/Controllers/OrdersController/OrdersController.cs
+++ TheCoffeeCatStore/Controllers/OrdersController/OrdersController.cs
@@ -33,93 +33,87 @@ namespace TheCoffeeCatStore.Controllers.OrdersController
             return _order.GetOrders().ToList();
         }
 
-        //// GET: api/Orders/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Order>> GetOrder(Guid id)
-        //{
-        //  if (_context.Orders == null)
-        //  {
-        //      return NotFound();
-        //  }
-        //    var order = await _context.Orders.FindAsync(id);
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrder(Guid id)
+        {
+            if (_order.GetOrders() == null)
+            {
+                return NotFound();
+            }
+            var order =  _order.GetOrderById(id);
 
-        //    if (order == null)
-        //    {
-        //        return NotFound();
-        //    }
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-        //    return order;
-        //}
+            return order;
+        }
 
-        //// PUT: api/Orders/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutOrder(Guid id, Order order)
-        //{
-        //    if (id != order.OrderID)
-        //    {
-        //        return BadRequest();
-        //    }
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public IActionResult PutOrder(Guid id, Order order)
+        {
+            if (_order.GetOrders() == null)
+            {
+                return BadRequest();
+            }
 
-        //    _context.Entry(order).State = EntityState.Modified;
+            try
+            {
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!OrderExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+                _order.UpdateOrder(order);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_order.GetOrderById(id)==null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
-        //// POST: api/Orders
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Order>> PostOrder(Order order)
-        //{
-        //  if (_context.Orders == null)
-        //  {
-        //      return Problem("Entity set 'TheCoffeeStoreDBContext.Orders'  is null.");
-        //  }
-        //    _context.Orders.Add(order);
-        //    await _context.SaveChangesAsync();
+        // POST: api/Orders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public ActionResult<Order> PostOrder(Order order)
+        {
+            if (_order.GetOrders() == null)
+            {
+                return Problem("Order is null.");
+            }
+            _order.AddNew(order);
 
-        //    return CreatedAtAction("GetOrder", new { id = order.OrderID }, order);
-        //}
+            return CreatedAtAction("GetOrder", new { id = order.OrderID }, order);
+        }
 
-        //// DELETE: api/Orders/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteOrder(Guid id)
-        //{
-        //    if (_context.Orders == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var order = await _context.Orders.FindAsync(id);
-        //    if (order == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(Guid id)
+        {
+            if (_order.GetOrders() == null)
+            {
+                return NotFound();
+            }
+            var order = _order.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-        //    _context.Orders.Remove(order);
-        //    await _context.SaveChangesAsync();
+            _order.ChangeOrderStatus(order);
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
-        //private bool OrderExists(Guid id)
-        //{
-        //    return (_context.Orders?.Any(e => e.OrderID == id)).GetValueOrDefault();
-        //}
+      
     }
 }
