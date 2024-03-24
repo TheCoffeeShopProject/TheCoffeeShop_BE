49ea89b13532a2b2303e7aa0b3ccde90179ef478
Duy1408 <duynguyen08142001@gmail.com>
2024-03-25 00:07:58 +0700

    innit-payment

---
+++ TheCoffeeCatService/IServices/IVnPayService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheCoffeeCatDAO.DAOs;
+using Microsoft.AspNetCore.Http;
+namespace TheCoffeeCatService.IServices
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+        PaymentInformationModel GetPaymentModelFromCache(Guid userId);
+    }
+}
